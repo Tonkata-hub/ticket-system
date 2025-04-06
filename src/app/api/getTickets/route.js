b1ad816b8dc81beda0716ea1ffe6999494342070
Tonkata-hub <tonyyankOV07@gmail.com>
2025-04-06 21:15:59 +0300

    Improved getTickets api endpoint to return based on isAdmin status

--- src/app/api/getTickets/route.js
+++ src/app/api/getTickets/route.js
@@ -1,11 +1,24 @@
 // app/api/getTickets/route.js
+import { decrypt } from "@/lib/session";
 import Ticket from "@/models/Ticket";
+import { cookies } from "next/headers";
+import User from "@/models/User";
 
 export async function GET() {
-    try {
-        const tickets = await Ticket.findAll();
-        return Response.json({ tickets });
-    } catch (error) {
-        return Response.json({ error: "Failed to fetch tickets" }, { status: 500 });
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get("session")?.value;
+    const session = sessionCookie && await decrypt(sessionCookie);
+
+    if (!session?.userId) {
+        return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const user = await User.findByPk(session.userId);
+    const isAdmin = user?.role === "admin";
+
+    const tickets = isAdmin
+        ? await Ticket.findAll()
+        : await Ticket.findAll({ where: { created_by: user.email } });
+
+    return Response.json({ tickets });
 }
